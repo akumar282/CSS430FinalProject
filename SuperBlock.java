9d8fca3200330ee4e8f992c65bf0d2e39a6a9a89
Semesus <devinzc@gmail.com>
2023-03-11 15:39:38 -0800

    Add files via upload
    fixed typo in class declaration

--- SuperBlock.java
+++ SuperBlock.java
@@ -1,4 +1,4 @@
-lass SuperBlock { 
+class SuperBlock { 
       public int totalBlocks; // the number of disk blocks 
       public int totalInodes; // the number of inodes 
       public int freeList;    // the block number of the free list's head 
