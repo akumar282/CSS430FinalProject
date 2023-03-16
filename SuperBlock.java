789683bf18dd3eb49de35489f62e2a4c77f9deef
Semesus <devinzc@gmail.com>
2023-03-15 18:56:10 -0700

    Add files via upload

--- SuperBlock.java
+++ SuperBlock.java
@@ -7,19 +7,48 @@ class SuperBlock {
 //While you have only three data members, you need to implement the following methods:  
    
     public SuperBlock( int diskSize ) { 
-        // read the superblock from disk.   
+        // read the superblock from disk.
+        byte[] superBlock = new byte[Disk.blockSize];   
+        SysLib.rawread(0, superBlock);
+        totalBlocks = SysLib.bytes2int(superBlock, 0);
+        totalInodes = SysLib.bytes2int(superBlock, 4);
+        freeList = SysLib.bytes2int(superBlock, 8);
         // check disk contents are valid. 
-        // if invalid, call format( ).                                                                                        
+        if(totalBlocks == diskSize && totalInodes > 0 && freeList >= 2) {
+            // disk contents valid
+            return;
+        } else {
+        // if invalid, call format( ).                                                                                       
+            totalBlocks = diskSize;
+            format(defaultInodeBlocks);
+        } 
     } 
     void sync( ) { 
-        // write back in-memory superblock to disk: SysLib.rawwrite( 0, superblock );   
+        // write back in-memory superblock to disk: SysLib.rawwrite( 0, superblock ); 
+        byte[] superBlock = new byte[Disk.blockSize];
+        SysLib.int2bytes( totalBlocks, superBlock, 0 );
+        SysLib.int2bytes( inodeBlocks, superBlock, 4 );
+        SysLib.int2bytes( freeList, superBlock, 8 );
+        SysLib.rawwrite( 0, superBlock );
+        SysLib.cerr( "Superblock synchronized\n" ); 
     } 
  
     void format( int files ) { 
         // initialize the superblock  
         // initialize each inode and immediately write it back to disk 
         // initialize free blocks 
+        // superblock: starts from #0
+        // inode:      starts from #1
+        // freelist:   starts from at least #2
+        // initialize each free block
+        for ( int i = freeList; i < totalBlocks; i++ ) {
+           // zero initialization
+           // let it point to the next blk
+           // write it back to disk
+        }
+        sync( );
     } 
+    
     public int getFreeBlock( ) { 
         // get a new free block from the freelist                                                                                 
         return freeBlockNumber; 
