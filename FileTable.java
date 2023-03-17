e1a0242c8efba466c3cc5174c41c547568f5bcee
akumar282 <abhi.kumar7567@gmail.com>
2023-03-16 21:36:06 -0700

    push

--- FileTable.java
+++ FileTable.java
@@ -1,51 +1,104 @@
-   public class FileTable { 
- 
-      private Vector table;         // the actual entity of this file table 
-      private Directory dir;        // the root directory  
- 
-      public FileTable( Directory directory ) { // constructor 
-         table = new Vector( );     // instantiate a file (structure) table 
-         dir = directory;           // receive a reference to the Director 
-      }                             // from the file system 
- 
-      // major public methods 
-      public synchronized FileTableEntry falloc( String filename, String mode ) { 
-        short iNumber = -1;
-        Inode inode = null;
-         // allocate a new file (structure) table entry for this file name 
-         while(true) {       
-            // allocate/retrieve and register the corresponding inode using dir 
-            iNumber = (filename.equals("/") ? 0 : dir.namei(filename);
+import java.util.*;
+
+public class FileTable {
+   private final static int UNUSED = 0; // inode.flag statuses
+   private final static int USED = 1;
+   private final static int READ = 2;
+   private final static int WRITE = 3;
+   private final static int DELETE = 4;
+   private Vector<FileTableEntry> table; // the actual entity of this file table
+   private Directory dir; // the root directory
+
+   public FileTable(Directory directory) { // constructor
+      table = new Vector<FileTableEntry>(); // instantiate a file (structure) table
+      dir = directory; // receive a reference to the Director
+   } // from the file system
+     // major public methods
+
+   public synchronized FileTableEntry falloc(String filename, String mode) {
+      // allocate a new file (structure) table entry for this file name
+      // allocate/retrieve and register the corresponding inode using dir
+      // increment this inode's count
+      // immediately write back this inode to the disk
+      // return a reference to this file (structure) table entry
+      short iNumber = -1;
+      Inode inode = null;
+
+      while (true) {
+         iNumber = (filename.equals("/") ? 0 : dir.namei(filename));
+         SysLib.cout("iNumber: " + iNumber + " mode: " + mode);
+         if (iNumber >= 0) {
             inode = new Inode(iNumber);
-            if(mode.compareTo("r") == 0) {
-                break;
-            } else if(mode.compareTo("w") == 0 || mode.compareTo("w+") == 0) {
-                try {
-                    wait()
-                } catch(InterruptedException e) {} // possibly incorrect implementation
-            } else if(mode.compareTo("a") == 0) {
-                iNumber = -1; // no more open
-                return null;
+            if (mode.compareTo("r") == 0) {
+               if ((inode.flag == UNUSED) || (inode.flag == USED)) {
+                  inode.flag = USED;
+                  break;
+               }
+               try {
+                  wait();
+               } catch (InterruptedException e) {
+               }
+            } else {
+
+               if ((inode.flag == UNUSED) || (inode.flag == WRITE)) {
+                  inode.flag = READ;
+                  break;
+               }
+               if ((inode.flag == USED) || (inode.flag == READ)) {
+                  inode.flag = WRITE;
+                  break;
+               }
+               try {
+                  wait();
+               } catch (InterruptedException e) {
+               }
             }
-         // increment this inode's count 
-         inode.count++;
-         // immediately write back this inode to the disk 
-         inode.toDisk(iNumber);
-         // return a reference to this file (structure) table entry 
-         FileTableEntry e = new FileTableEntry(inode, iNumber, mode);
-         table.addElement(e);
-         return e;
-      } 
- 
-      // TODO
-      public synchronized boolean ffree( FileTableEntry e ) { 
-         // receive a file table entry reference 
-         // save the corresponding inode to the disk 
-         // free this file table entry. 
-         // return true if this file table entry found in my table 
-      } 
- 
-      public synchronized boolean fempty( ) { 
-         return table.isEmpty( );  // return if table is empty  
-      }                            // should be called before starting a format 
+         } else if (mode.compareTo("w") != 0) {
+            iNumber = dir.ialloc(filename);
+            inode = new Inode(iNumber);
+            inode.flag = 2;
+            break;
+         }
+      }
+      inode.count++;
+      inode.toDisk(iNumber);
+      FileTableEntry e = new FileTableEntry(inode, iNumber, mode);
+      table.addElement(e);
+      return e;
    }
+
+   public synchronized boolean ffree(FileTableEntry e) {
+      // receive a file table entry reference
+      // save the corresponding inode to the disk
+      // free this file table entry.
+      // return true if this file table entry found in my table
+      if (table.removeElement(e)) {
+         Inode inode = e.inode;
+         --inode.count;
+         switch (e.inode.flag) {
+            case 1: // read
+               e.inode.flag = 0;
+               break;
+            case 2: // write
+               e.inode.flag = 0;
+               break;
+            case 3: // read/write
+               e.inode.flag = 3;
+               break;
+            case 4: // delete
+               e.inode.flag = 3;
+               break;
+         }
+
+         e.inode.toDisk(e.iNumber);
+         notify();
+         return true;
+      }
+      return false;
+   }
+
+   public synchronized boolean fempty() {
+      // return if table is empty
+      return table.isEmpty();
+   } // should be called before starting a format
+}
--- SuperBlock.java
+++ SuperBlock.java
@@ -1,60 +1,84 @@
-class SuperBlock { 
-      public int totalBlocks; // the number of disk blocks 
-      public int totalInodes; // the number of inodes 
-      public int freeList;    // the block number of the free list's head 
-   } 
- 
-//While you have only three data members, you need to implement the following methods:  
-   
-    public SuperBlock( int diskSize ) { 
+import java.util.*;
+
+class SuperBlock {
+    public int totalBlocks; // the number of disk blocks
+    public int totalInodes; // the number of inodes
+    public int freeList; // the block number of the free list's head
+    private final int defaultInodeBlocks = 64;
+
+    public SuperBlock(int diskSize) {
         // read the superblock from disk.
-        byte[] superBlock = new byte[Disk.blockSize];   
+        // check disk contents are valid.
+        // if invalid, call format( ).
+        byte[] superBlock = new byte[Disk.blockSize];
         SysLib.rawread(0, superBlock);
         totalBlocks = SysLib.bytes2int(superBlock, 0);
         totalInodes = SysLib.bytes2int(superBlock, 4);
         freeList = SysLib.bytes2int(superBlock, 8);
-        // check disk contents are valid. 
-        if(totalBlocks == diskSize && totalInodes > 0 && freeList >= 2) {
-            // disk contents valid
+
+        if (totalBlocks == diskSize && totalInodes > 0 && freeList >= 2) {
             return;
+
         } else {
-        // if invalid, call format( ).                                                                                       
             totalBlocks = diskSize;
             format(defaultInodeBlocks);
-        } 
-    } 
-    void sync( ) { 
-        // write back in-memory superblock to disk: SysLib.rawwrite( 0, superblock ); 
-        byte[] superBlock = new byte[Disk.blockSize];
-        SysLib.int2bytes( totalBlocks, superBlock, 0 );
-        SysLib.int2bytes( inodeBlocks, superBlock, 4 );
-        SysLib.int2bytes( freeList, superBlock, 8 );
-        SysLib.rawwrite( 0, superBlock );
-        SysLib.cerr( "Superblock synchronized\n" ); 
-    } 
- 
-    void format( int files ) { 
-        // initialize the superblock  
-        // initialize each inode and immediately write it back to disk 
-        // initialize free blocks 
-        // superblock: starts from #0
-        // inode:      starts from #1
-        // freelist:   starts from at least #2
-        // initialize each free block
-        for ( int i = freeList; i < totalBlocks; i++ ) {
-           // zero initialization
-           // let it point to the next blk
-           // write it back to disk
         }
-        sync( );
-    } 
-    
-    public int getFreeBlock( ) { 
-        // get a new free block from the freelist                                                                                 
-        return freeBlockNumber; 
-    } 
- 
-    public boolean returnBlock( int oldBlockNumber ) { 
-        // return this old block to the free list. The list can be a stack.                                                                                                 
-        return true or false; 
     }
+
+    void sync() {
+        // write back in-memory superblock to disk: SysLib.rawwrite( 0, superblock );
+        byte[] superBlock = new byte[Disk.blockSize];
+        SysLib.int2bytes(totalBlocks, superBlock, 0);
+        SysLib.int2bytes(totalInodes, superBlock, 4);
+        SysLib.int2bytes(freeList, superBlock, 8);
+        SysLib.rawwrite(0, superBlock);
+        SysLib.cerr("we are in superblock sync\n");
+        SysLib.cerr("Superblock syncronized\n");
+    }
+
+    void format(int files) {
+        totalInodes = files;
+        for (short i = 0; i < totalInodes; i++) {
+            Inode node = new Inode();
+            node.flag = 0;
+            node.toDisk(i);
+        }
+        this.freeList = 2 + totalInodes * 32 / 512;
+        for (int i = this.freeList; i < this.totalBlocks; i++) {
+            byte[] Block = new byte[512];
+            for (int j = 0; j < 512; j++) {
+                Block[j] = 0;
+            }
+            SysLib.int2bytes(i + 1, Block, 0);
+            SysLib.rawwrite(i, Block);
+        }
+        this.sync();
+    }
+
+    public int getFreeBlock() {
+        // get a new free block from the freelist
+        int freeBlockNumber = this.freeList;
+        if (freeBlockNumber != -1) {
+            byte[] Block = new byte[512];
+            SysLib.rawread(freeBlockNumber, Block);
+            this.freeList = SysLib.bytes2int(Block, 0);
+            SysLib.int2bytes(0, Block, 0);
+            SysLib.rawwrite(freeBlockNumber, Block);
+        }
+        return freeBlockNumber;
+    }
+
+    public boolean returnBlock(int oldBlockNumber) {
+        // return this old block to the free list. The list can be a stack.
+        byte[] Block = new byte[512];
+        if (oldBlockNumber >= 0) {
+            for (int i = 0; i < 512; i++) {
+                Block[i] = 0;
+            }
+        }
+        SysLib.int2bytes(this.freeList, Block, 0);
+        SysLib.rawwrite(oldBlockNumber, Block);
+        this.freeList = oldBlockNumber;
+        return false;
+    }
+}
